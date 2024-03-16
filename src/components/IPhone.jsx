9b47786b7bb463bdf23384979aa713e5df72935e
Muhammad-Hassan31144 <31144@students.riphah.edu.pk>
2024-03-17 01:01:56 +0500

    minor change

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -10,7 +10,7 @@ import * as THREE from 'three';
 import React, { useEffect, useRef } from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
 
-function IPhone(props) {
+function Model(props) {
   const { nodes, materials } = useGLTF("/models/scene.glb");
 
   const texture = useTexture(props.item.img);
@@ -256,6 +256,6 @@ function IPhone(props) {
   );
 }
 
-export default IPhone;
+export default Model;
 
 useGLTF.preload("/models/scene.glb");
